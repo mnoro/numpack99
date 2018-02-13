df3b517bab32513d8bb2709ef4a9bc0e52e69251
mnoro <mnoro@yahoo.com>
2018-02-13 22:44:08 +0000

    New tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,5 @@
 library(testthat)
 library(numpack99)
 
-# test_check("numpack99")
+test_check("numpack99")
 
-context("Correct Calculations")
-
-test_that("The calculations are fine", {
-  expect_equal(factorial_loop(4), 24)
-})
---
+++ tests/testthat/test-factorial_loop_001.R
@@ -0,0 +1,8 @@
+context("Correct Calculations")
+
+test_that("The calculations are fine", {
+  expect_equal(factorial_loop(4), 24)
+  expect_equal(factorial_loop(3), 6)
+  expect_equal(factorial_loop(9), 362880)
+  expect_equal(factorial_loop(8), factorial(8))
+})
