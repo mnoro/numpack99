9bb19207fe2c531e9088f2aa78f56cd1e77a360b
mnoro <mnoro@yahoo.com>
2018-02-13 08:23:25 +0000

    Adding Friendly Numbers - part1

---
+++ R/friendly_numbers.R
@@ -0,0 +1,18 @@
+#' Calculates whether two integer numbers are friendly numbers
+#'
+#' Returns a TRUE/FALSE to determine whether two integer numbers are friendly
+#'
+#' @param num1 The first integer number
+#'
+#' @param num2 The second integer number
+#'
+#' @return A Logical TRUE/FALSE
+#'
+#' @examples
+#' \dontrun{
+#' friendly_numbers(12, 16)
+#' }
+#' @export
+friendly_numbers <- function(num1, num2){
+
+}
